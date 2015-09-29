8ebe708ce330ad233daeaa18a8db76a3751d7f74
Adolfo <adolfo.encinas@me.com>
2015-09-29 18:11:54 +0200

    Removed demo project

--- APTimeZones/APTimeZones.h
+++ APTimeZones/APTimeZones.h
@@ -15,5 +15,7 @@ FOUNDATION_EXPORT double APTimeZonesVersionNumber;
 FOUNDATION_EXPORT const unsigned char APTimeZonesVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <APTimeZones/PublicHeader.h>
-
+#import <APTimeZones/APTimeZones.h>
+#import <APTimeZones/CLLocation+APTimeZones.h>
+#import <APTimeZones/CLPlacemark+APTimeZones.h>
 
